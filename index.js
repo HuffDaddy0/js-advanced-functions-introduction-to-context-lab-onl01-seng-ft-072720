9c38da8ae134b05399a14f2354c80c5c52fcf6f5
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-11 13:16:41 -0500

    commit tree 3fec0ba57e9c4ea74373e64a0b35222d00b619bd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const createEmployeeRecord = function(array){
+    const [ firstName, familyName, title, payPerHour ] = array;
+    const attributes = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return Object.assign({}, attributes);
+};
+
+const createEmployeeRecords = function(arrays){
+    const records = arrays.map(createEmployeeRecord)
+    return records
+};
